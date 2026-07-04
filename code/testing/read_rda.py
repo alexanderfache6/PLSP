573e869c0ac4e52a73a52dcb151999cce86531fc
Alexander Fache <alexander.fache@gmail.com>
2026-07-04 12:41:44 -0400

    updated read_rda.py

--- code/testing/read_rda.py
+++ code/testing/read_rda.py
@@ -1,13 +1,14 @@
 import pyreadr
 
 # result = pyreadr.read_r('/projectnb/planet/PLSP/data_paper/data/comp_hls/hls_3by3/50PCGI_2017_US-SRM__Santa_Rita_Mesquite.rda')
-result = pyreadr.read_r('/projectnb/modislc/users/fache/data/planet/chunks/Walnut_Gulch_Kendall_Grasslands/temp/001/250101.rda')
+# result = pyreadr.read_r('/projectnb/modislc/users/fache/data/planet/chunks/Walnut_Gulch_Kendall_Grasslands/temp/001/250101.rda')
+result = pyreadr.read_r('/projectnb/modislc/users/fache/data/planet/chunks/Walnut_Gulch_Kendall_Grasslands/chunk_001.rda')
 
-
-print(len(list(result.values())))
+print("number of bands:", len(list(result.values())))
 dfs = list(result.values())
 
 for df in dfs:
+    print("="*40)
     print(df.columns)
-    print(df.shape)
-    print(df.head(10))
+    print(f"{df.shape=}")
+    print(df.head(5))
